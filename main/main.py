4de50ccf76d5a06841caf71cab2a4387d1c0ca1f
Joshua Snyder <josh@Joshuas-Mac-Pro-31.local>
2020-10-31 15:42:31 -0700

    adding config to init and run of main

--- main/main.py
+++ main/main.py
@@ -1,8 +1,11 @@
 
 
 class main:
-    def __init__(self):
+    def __init__(self, config = None):
+        self._config = config
         print("main init")
 
-    def run(self):
+    def run(self, config = None):
         print("Running!")
+        print(config)
+        print(self._config)
